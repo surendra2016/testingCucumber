f8bb984c35e975046ce81b0b56f1b558608d9939
surendra2016 <surendra@mail.uni-paderborn.de>
2018-08-04 17:57:01 +0200

    initial commit

---
+++ com.testing.cucumber/resources/features/HomePage.feature
@@ -0,0 +1,7 @@
+Feature: Homepage
+This feature verifies the functionality on  Homepage
+ 
+Scenario: Check that main elements on Homepage are displayed
+Given I launch Chrome browser
+When I open Google Homepage
+Then I verify that the page displays search text box
---
+++ com.testing.cucumber/src/main/java/testRunners/TestRunner_HomePage.java
@@ -0,0 +1,11 @@
+package testRunners;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(features="resources/features", glue="")
+public class TestRunner_HomePage {
+
+}
